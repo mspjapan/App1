a8794add4de5542f69af5af2bb598d85cc00771c
Y&u <willful.slight@gmail.com>
2014-09-17 00:15:08 +0900

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	App1/App1/App1.Windows/obj/Debug/App.g.cs
    	App1/App1/App1.Windows/obj/Debug/MainPage.g.cs
    	App1/App1/App1.Windows/obj/Debug/MainPage.g.i.cs
    	App1/App1/App1.Windows/obj/Debug/XamlSaveStateFile.xml

--- App1/App1/App1.Windows/HttpClientBasic.cs
+++ App1/App1/App1.Windows/HttpClientBasic.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http.HttpClient;
+
 
 namespace App1
 {
     class HttpClientBasic
     {
+        public void PostInitialize(string url)
+        {
+          
+
+        }
     }
 }
